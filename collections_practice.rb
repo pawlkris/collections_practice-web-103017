eaeb09a8f1844b3ae25b321a39aa0087819ec3a6
Paul Kristapovich <paulkristapovich@gmail.com>
2017-10-31 13:04:30 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort { |x,y| y <=> x }
+end
+
+def sort_array_char_count(array)
+  array.sort_by(&:length)
+end
+
+def swap_elements(array)
+  array[1],array[2] = array[2],array[1]
+  return array
+end
+
+def reverse_array(array)
+  array.reverse!
+end
+
+def kesha_maker(array)
+  array.collect {|x| x.sub!(x[2],"$")}
+end
+
+def find_a(array)
+  array.select {|x| x.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject(:+)
+end
+
+def add_s(array)
+  array = array.collect do |x|
+    if x != array[2]
+      x=x.insert(-1,"s")
+    end
+  end
+end
